ce80f89a2ae8d848a128ceacce7da88e3eda24ac
Per Kofod Hansen <per@kofod-hansen.com>
2017-11-12 18:00:18 +0100

    Converted to Python 2.7 - since raspberry PI does not have cv2 in python 3.5

--- countdownwatch.py
+++ countdownwatch.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 from datetime import datetime, time
 import sys, pygame
-import configparser
+import ConfigParser
 #import matplotlib.pyplot as plt
 
 ''' configuration '''
@@ -26,42 +26,44 @@ pygame.init()
 clock = pygame.time.Clock()
 pygame.key.set_repeat(500, 50)
 
+#List available modes
+for mode in pygame.display.list_modes(0, pygame.FULLSCREEN):
+    print("mode: " + str(repr(mode)))
+
 #screen = pygame.display.set_mode(size, pygame.FULLSCREEN)
 screen = pygame.display.set_mode(size)
 font = pygame.font.Font('LCD.ttf', 170)
 cursor = [[0, 0], [width, 0], [0, height], [width, height]]
+
 cursor_idx = 0
 cursor_on = False
 
 first_run = True
 
 def save_settings():
-    config = configparser.ConfigParser()
+    config = ConfigParser.ConfigParser()
 
-    config['DEADLINE'] = {
-        'day' : deadline.day,
-        'month': deadline.month,
-        'year': deadline.year,
-        'hour': deadline.hour,
-        'minute': deadline.minute,
-    }
+    config.add_section('DEADLINE')
+    config.set('DEADLINE', 'day', deadline.day)
+    config.set('DEADLINE', 'month', deadline.month)
+    config.set('DEADLINE', 'year', deadline.year)
+    config.set('DEADLINE', 'hour', deadline.hour)
+    config.set('DEADLINE', 'minute', deadline.minute)
 
-    config['COLOR_FORGROUND'] = {
-        'red' : color_fg[0],
-        'green' : color_fg[1],
-        'blue' : color_fg[2]
-    }
+    config.add_section('COLOR_FORGROUND')
+    config.set('COLOR_FORGROUND', 'red', color_fg[0])
+    config.set('COLOR_FORGROUND', 'green', color_fg[1])
+    config.set('COLOR_FORGROUND', 'blue', color_fg[2])
 
-    config['CURSORS'] = {
-        'c0x': cursor[0][0],
-        'c0y': cursor[0][1],
-        'c1x': cursor[1][0],
-        'c1y': cursor[1][1],
-        'c2x': cursor[2][0],
-        'c2y': cursor[2][1],
-        'c3x': cursor[3][0],
-        'c3y': cursor[3][1],
-    }
+    config.add_section('CURSORS')
+    config.set('CURSORS', 'c0x', cursor[0][0])
+    config.set('CURSORS', 'c0y', cursor[0][1])
+    config.set('CURSORS', 'c1x', cursor[1][0])
+    config.set('CURSORS', 'c1y', cursor[1][1])
+    config.set('CURSORS', 'c2x', cursor[2][0])
+    config.set('CURSORS', 'c2y', cursor[2][1])
+    config.set('CURSORS', 'c3x', cursor[3][0])
+    config.set('CURSORS', 'c3y', cursor[3][1])
 
     print("saving")
     with open(config_filename, 'w') as configfile:
@@ -69,31 +71,33 @@ def save_settings():
 
 
 def load_settings():
-    config = configparser.ConfigParser()
+    global deadline
+
+    config = ConfigParser.ConfigParser()
 
     print("loading")
 
     config.read(config_filename)
 
-    day = int(config['DEADLINE']['day'])
-    month = int(config['DEADLINE']['month'])
-    year = int(config['DEADLINE']['year'])
-    hour = int(config['DEADLINE']['hour'])
-    minute = int(config['DEADLINE']['minute'])
+    day = int(config.get('DEADLINE', 'day'))
+    month = int(config.get('DEADLINE', 'month'))
+    year = int(config.get('DEADLINE', 'year'))
+    hour = int(config.get('DEADLINE', 'hour'))
+    minute = int(config.get('DEADLINE', 'minute'))
     deadline = datetime(day=day, month=month, year=year, hour=hour, minute=minute)
 
-    color_fg[0] = int(config['COLOR_FORGROUND']['red'])
-    color_fg[1] = int(config['COLOR_FORGROUND']['green'])
-    color_fg[2] = int(config['COLOR_FORGROUND']['blue'])
+    color_fg[0] = int(config.get('COLOR_FORGROUND', 'red'))
+    color_fg[1] = int(config.get('COLOR_FORGROUND', 'green'))
+    color_fg[2] = int(config.get('COLOR_FORGROUND', 'blue'))
 
-    cursor[0][0] = int(config['CURSORS']['c0x'])
-    cursor[0][1] = int(config['CURSORS']['c0y'])
-    cursor[1][0] = int(config['CURSORS']['c1x'])
-    cursor[1][1] = int(config['CURSORS']['c1y'])
-    cursor[2][0] = int(config['CURSORS']['c2x'])
-    cursor[2][1] = int(config['CURSORS']['c2y'])
-    cursor[3][0] = int(config['CURSORS']['c3x'])
-    cursor[3][1] = int(config['CURSORS']['c3y'])
+    cursor[0][0] = int(config.get('CURSORS', 'c0x'))
+    cursor[0][1] = int(config.get('CURSORS', 'c0y'))
+    cursor[1][0] = int(config.get('CURSORS', 'c1x'))
+    cursor[1][1] = int(config.get('CURSORS', 'c1y'))
+    cursor[2][0] = int(config.get('CURSORS', 'c2x'))
+    cursor[2][1] = int(config.get('CURSORS', 'c2y'))
+    cursor[3][0] = int(config.get('CURSORS', 'c3x'))
+    cursor[3][1] = int(config.get('CURSORS', 'c3y'))
 
 ''' Find the smallest size of a box containing the 4 corners 
     (topleft, topright, botleft, botright) (flipping not supported)
@@ -105,7 +109,7 @@ def find_corner_rectsize(cursors):
     return (width, height)
 
 while 1:
-    clock.tick(60) # 30 fps
+    clock.tick(30) # 30 fps
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
@@ -132,6 +136,11 @@ while 1:
                 load_settings()
 
     current_timer_s = (deadline - datetime.now()).seconds
+    if(deadline > datetime.now()):
+        deadline_reached = False
+    else:
+        deadline_reached = True
+
     current_timer_hours = current_timer_s // 3600
     # remaining seconds
     current_timer_s = current_timer_s - (current_timer_hours * 3600)
@@ -140,7 +149,11 @@ while 1:
     # remaining seconds
     current_timer_s = current_timer_s - (current_timer_minutes * 60)
 
-    current_timer_string = '%02d:%02d:%02d' % (current_timer_hours, current_timer_minutes, current_timer_s)
+    if(not deadline_reached):
+        current_timer_string = '%02d:%02d:%02d' % (current_timer_hours, current_timer_minutes, current_timer_s)
+    else:
+        current_timer_string = '00:00:00'
+
     current_timer = font.render(current_timer_string, True, color_fg, black)
 
     cv_current_timer = pygame.surfarray.array3d(current_timer)
